5f8b19689ba0f8ed2a5682c37e2c0ab895877ad8
Cole Cofer <coleocofer@gmail.com>
2025-11-03 21:19:10 -0800

    platforms galore

--- game-off-2025/Entities/Particles/cave_particles.gd
+++ game-off-2025/Entities/Particles/cave_particles.gd
@@ -2,27 +2,28 @@ extends CPUParticles2D
 
 # Majestic floating cave particles that flow across the screen
 
-@onready var camera: Camera2D
-
 func _ready():
 	# Make sure particles are emitting
 	emitting = true
 	randomize()
 
-	# Find the camera to follow
+	# Find the camera to attach to
 	await get_tree().process_frame
 	var player = get_tree().get_first_node_in_group("player")
+	var camera: Camera2D = null
+
 	if player:
 		camera = player.get_node_or_null("Camera2D")
 		if not camera:
 			# Try to find any camera in the scene
 			camera = get_viewport().get_camera_2d()
 
-func _process(_delta):
 	if camera:
-		# Position particles to emit from right side of visible screen
-		var camera_pos = camera.get_screen_center_position()
-		var viewport_size = get_viewport_rect().size
+		# Reparent to camera so particles follow it
+		var old_parent = get_parent()
+		old_parent.remove_child(self)
+		camera.add_child(self)
 
-		# Emit from right edge of screen, centered vertically
-		position = camera_pos + Vector2(viewport_size.x * 0.6, 0)
+		# Position to emit from right side of screen
+		var viewport_size = get_viewport_rect().size
+		position = Vector2(viewport_size.x * 0.6, 0)

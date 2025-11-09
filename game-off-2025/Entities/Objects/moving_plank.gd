a4c38ad8454713a8438b2e2cdb8d5003679d6f97
Cole Cofer <coleocofer@gmail.com>
2025-11-09 08:25:33 -0800

    Made planks move up and down

--- game-off-2025/Entities/Objects/moving_plank.gd
+++ game-off-2025/Entities/Objects/moving_plank.gd
@@ -1,19 +1,30 @@
 extends AnimatableBody2D
 
+enum MovementDirection {
+	HORIZONTAL,
+	VERTICAL
+}
+
+@export var movement_direction: MovementDirection = MovementDirection.HORIZONTAL
 @export var move_speed: float = 50.0
 @export var move_distance: float = 100.0
 @export_range(0.0, 1.0, 0.01) var time_offset: float = 0.0  ## Offset as percentage of cycle (0-1)
 
-var left_position: float
-var direction: int = 1  # 1 for right, -1 for left
+var start_position: float
+var direction: int = 1  # 1 for right/down, -1 for left/up
 var time_elapsed: float = 0.0
 var cycle_time: float
 
 func _ready():
-	left_position = global_position.x  # Starting position is the leftmost point
+	# Set starting position based on movement direction
+	if movement_direction == MovementDirection.HORIZONTAL:
+		start_position = global_position.x  # Starting position is the leftmost point
+	else:
+		start_position = global_position.y  # Starting position is the topmost point
+
 	sync_to_physics = true
 
-	# Calculate how long one full cycle takes (left to right and back)
+	# Calculate how long one full cycle takes (start to end and back)
 	cycle_time = (move_distance * 2.0) / move_speed
 
 	# Apply time offset to start at a different point in the cycle
@@ -36,12 +47,24 @@ func _update_position_from_time():
 	var half_cycle = cycle_time / 2.0
 
 	if time_elapsed < half_cycle:
-		# Moving right (first half of cycle)
+		# Moving forward (first half of cycle - right for horizontal, down for vertical)
 		var progress = time_elapsed / half_cycle
-		global_position.x = left_position + (move_distance * progress)
+		var new_position = start_position + (move_distance * progress)
+
+		if movement_direction == MovementDirection.HORIZONTAL:
+			global_position.x = new_position
+		else:
+			global_position.y = new_position
+
 		direction = 1
 	else:
-		# Moving left (second half of cycle)
+		# Moving backward (second half of cycle - left for horizontal, up for vertical)
 		var progress = (time_elapsed - half_cycle) / half_cycle
-		global_position.x = left_position + move_distance - (move_distance * progress)
+		var new_position = start_position + move_distance - (move_distance * progress)
+
+		if movement_direction == MovementDirection.HORIZONTAL:
+			global_position.x = new_position
+		else:
+			global_position.y = new_position
+
 		direction = -1
